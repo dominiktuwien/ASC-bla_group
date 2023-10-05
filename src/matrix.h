8ffd6961b5203b7c85e61bb068e3ee9653911e15
Dominik Poppe <e12008560@student.tuwien.ac.at>
2023-10-05 16:17:38 +0200

    matrix(versuch)

--- src/matrix.h
+++ src/matrix.h
@@ -1,15 +1,28 @@
-//#ifndef FILE_VECTOR_H
-//#define FILE_VECTOR_H
+#ifndef FILE_MATRIX_H
+#define FILE_MATRIX_H
 
 #include <iostream>
 
 enum ORDERING { ColMajor, RowMajor };
 template <typename T, ORDERING ORD>
     class Matrix {
-        size_t row_lenght_;
-        size_t col_lenght_;
-        T * data_;
-
+        std::Vector(T) data_;
+        size_t height_;
+        size_t widht_;
+        
+        
     public:
-    
+
+    Matrix (size_t col_lenght, size_t row_lenght)
+        : row_lenght_(row_lenght), col_lenght_(col_lenght), data_(new T);
+
+    Matrix (const Matrix & m)
+        : Matrix(m.col_lenght_(),m.row_lenght_())
+    {
+        *this = m;
+    }
     };
+
+
+
+#endif
