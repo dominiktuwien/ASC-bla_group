28ec619486bcfe0e1627d925483decaff19de73f
SubDune <eichler.daniel01@gmail.com>
2023-10-10 16:24:35 +0200

    Kommentare und renaming

--- src/matrix.h
+++ src/matrix.h
@@ -1,4 +1,4 @@
-#ifndef FILE_MATRIX_H //to avoid double declaration if "matrix" is opened from multiple files
+#ifndef FILE_MATRIX_H
 #define FILE_MATRIX_H
 
 #include <iostream>
@@ -11,38 +11,38 @@ template <typename T, ORDERING ORD>
         //ORD* order_;
         T* data_;
         size_t height_;
-        size_t width_;
-        size_t elements_;
+        size_t widht_;
+        size_t n_of_elements_;   //kann man hier nicht direkt height_ * width_ für alle Matrizen machen? -Da
         
     public:
 
-    Matrix()
-        : height_(1), width_(1), elements_(1), data_(new T[elements_]){data_[0] = 0.0; }
+    Matrix()    //Standard-Konstruktor --1x1 Matrix mit Nullelement
+        : height_(1), widht_(1), n_of_elements_(1), data_(new T[n_of_elements_]){data_[0] = 0.0; }
 
-    Matrix (size_t height, size_t width)
-        : height_(height), width_(width), elements_(height_*width_), data_(new T[elements_]) 
-    { for (size_t i = 0; i < elements_; i++)
+    Matrix (size_t height, size_t widht)
+        : height_(height), widht_(widht), n_of_elements_(height_*widht_), data_(new T[n_of_elements_]) 
+    { for (size_t i = 0; i < n_of_elements_; i++)    //füllt Matrix Element für Element mit 0
         data_[i] = 0.0;
     }
 
-    Matrix (size_t height, size_t width, const T* inputdata)
-        : height_(height), width_(width), elements_(height_*width_), data_(new T[elements_]) 
-    { for (size_t i = 0; i < elements_; i++)
-        data_[i] = inputdata[i];
+    Matrix (size_t height, size_t widht, const T* imputdata)
+        : height_(height), widht_(widht), n_of_elements_(height_*widht_), data_(new T[n_of_elements_]) 
+    { for (size_t i = 0; i < n_of_elements_; i++)
+        data_[i] = imputdata[i];
     }
 
-    Matrix (const Matrix & m)
+    Matrix (const Matrix & m)       //wenn Matrix übergeben, muss nix verändert werden?
         : Matrix(m.col_lenght_(),m.row_lenght_())
     {
         *this = m;
     }
 
     Matrix (const Matrix && m)
-        : height_(0), width_(0), elements_(0), data_(nullptr)
+        : height_(0), widht_(0), n_of_elements_(0), data_(nullptr)
     {
         //std::swap(height_, m.height_);
         //std::swap(widht_, m.widht_);
-        //std::swap(elements_, m.elements_);
+        //std::swap(elements_, m.n_of_elements_);
         //std::swap(data_, m.data_);
     }
 
